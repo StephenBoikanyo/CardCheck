1617c4aef3f624df7097ba23b191c6e6c1a5f7b8
Stephen Boikanyo <StephenBoikanyo69@Gmail.com>
2023-06-15 15:12:47 +0200

    Initial commit

---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:card_check/app/app.dart';
+import 'package:card_check/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:card_check/app/app.dart';
+import 'package:card_check/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
