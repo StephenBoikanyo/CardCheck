0b82667771ce56356319b2bcc3ac165e6418baea
Stephen Boikanyo <StephenBoikanyo69@Gmail.com>
2023-06-21 15:19:35 +0200

    updated CardUtils for scard scanning

--- lib/services/card_utils.dart
+++ lib/services/card_utils.dart
@@ -1,6 +1,10 @@
-
+import 'package:credit_card_scanner/credit_card_scanner.dart';
+import 'package:flutter/cupertino.dart';
 
 class CardUtils {
+
+
+  //Function to determine card provider
   static String getCardProvider(String cardNumber) {
     if (cardNumber.startsWith('34') || cardNumber.startsWith('37')) {
       return 'Amex';
@@ -16,6 +20,7 @@ class CardUtils {
     return 'Unknown';
   }
 
+  // function to validate card
   static bool isValidCardLength(String provider, String cardNumber) {
     int length = cardNumber.length;
     if (provider == 'Amex') {
@@ -25,4 +30,20 @@ class CardUtils {
     }
     return false;
   }
+
+  // Function to scan a card
+  Future<void> scanCard( TextEditingController cardNumberController,TextEditingController expiryDateController) async {
+    CardScanOptions scanOptions = CardScanOptions(
+      scanCardHolderName: false,
+    );
+
+    CardDetails? cardDetails = await CardScanner.scanCard(scanOptions: scanOptions);
+
+    if (cardDetails != null) {
+      cardNumberController.text = cardDetails.cardNumber;
+      expiryDateController.text = cardDetails.expiryDate;
+
+    }
+  }
+
 }
