cada27229fdc8aecf491ac3e1c0e1c09ddd01594
Stephen Boikanyo <StephenBoikanyo69@Gmail.com>
2023-06-22 02:33:23 +0200

    Refactored class for country list service

--- lib/services/country_list.dart
+++ lib/services/country_list.dart
@@ -1,61 +1,53 @@
 
 
 class CountryList {
-  final List<String> allCountries = [
-    'Brazil',
-    'Russia',
-    'India'
-    'South Africa',
-    'Botswana',
-    'Afghanistan',
-    'Albania',
-    'Algeria',
-    'Andorra',
-    'Angola',
-    'Antigua and Barbuda',
-    'Argentina',
-    'Armenia',
-    'Australia',
-    'Austria',
-    'Azerbaijan',
-    'Bahamas',
-    'Bahrain',
-    'Bangladesh',
-    'Barbados',
-    'Belarus',
-    'Belgium',
-    'Belize',
-    'Benin',
-    'Bhutan',
-    'Bolivia',
-    'Bosnia and Herzegovina',
-    'Botswana',
-    'Brazil',
-    'Brunei',
-    'Bulgaria',
-  ];
 
-  List<String> countries = [];
+  CountryList() {
+    countries = [
+       'Brazil',
+      'Russia',
+       'India',
+      'China',
+      'South Africa',
+      'Albania',
+      'Argentina',
+      'Australia',
+      'Brazil',
+      'Canada',
+      'China',
+      'Denmark',
+      'Egypt',
+      'France',
+      'Germany',
+      'India',
+      'Italy',
+      'Japan',
+      'Mexico',
+      'Netherlands',
+      'Peru',
+      'Russia',
+      'Spain',
+      'United Kingdom',
+      'Afghanistan',
+    ];
 
-    Future<List<String>> getCountryList() async {
-    await Future.delayed(Duration(seconds: 2)); // Simulating a delay to show async nature of Dart
+    bannedCountries = countries.sublist(15);
 
-    return countries;
   }
 
-  void generateCountryList() {
-    countries = List.from(allCountries);
-    if (countries.length > 15) {
-      for (int i = 15; i < countries.length; i++) {
-        countries[i] = 'Banned ${countries[i]}';
-      }
-    }
-    countries = countries.sublist(0, (20 < countries.length ? 20 : countries.length) as int);
-  }
 
-  void appendCountry(String country) {
-    if (countries.length < 20) {
-      countries.add(country);
+  void addCountry(String country) {
+    countries.add(country);
+    if (countries.indexOf(country) >= 15) {
+      bannedCountries.add(country);
     }
   }
+
+  void removeCountry(String country) {
+    countries.remove(country);
+    bannedCountries.remove(country);
+  }
+
+  late List<String> countries;
+  late List<String> bannedCountries;
 }
